7ec73ca00306667f5c9bced44639adf709c1ad5e
MohamadWattad <abotalebwattad@gmail.com>
2024-07-13 17:48:31 +0300

    BSPMS2427-6 <message> , BSPMS2427-7 <message> , BSPMS2427-9 <message> , BSPMS2427-10 <message>,  BSPMS2427-12 <message>, BSPMS2427-13 <message>

--- APP.js
+++ APP.js
@@ -5,7 +5,7 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 const nodemailer = require('nodemailer');
 const passwordValidator = require("password-validator");
-// const User = require('./Database/User.js').User;
+const User = require('./Database/User.js').User;
 // const req1 = require('./Database/sreq.js').reqs;
 // const req2 = require('./Database/freq.js').req2;
 const contact = require('./Database/cont.js').contact;
@@ -50,6 +50,127 @@ app.get('/contact', async(req, res) => {
 app.get('/HomePage', (req, res) => {
     res.render('HomePage');
 });
+app.get('/Sign-Up', (req, res) => {
+    res.render('Sign-Up');
+});
+
+app.get('/Sign-Up-Student', (req, res) => {
+    res.render('Sign-Up-Student');
+});
+app.get('/Sign-In', (req, res) => {
+    res.render('Sign-In');
+});
+app.post("/Sign-Up", async(req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+        const newUser = new User({
+            FirstName: req.body.FirstName,
+            LastName: req.body.LastName,
+            id: req.body.id,
+            password: hashedPassword,
+            email: req.body.email,
+            Gender: req.body.Gender,
+            Age: req.body.Age,
+            Phone: req.body.Phone,
+            Roll: req.body.Roll,
+            Birthdate: req.body.Birthdate
+        });
+
+        const existingUser = await User.findOne({ FirstName: req.body.FirstName });
+
+        if (existingUser) {
+            console.log("User already exists!");
+            return res.redirect("/Sign-Up");
+        }
+
+        if (!passwordSchema.validate(req.body.password)) {
+            console.log("Password validation failed");
+            return res.redirect("/Sign-Up");
+        }
+
+        await newUser.save();
+        console.log("Sign up successful");
+        return res.redirect('/Sign-In');
+
+    } catch (err) {
+        console.error('Error signing up:', err);
+        return res.redirect('/Sign-Up');
+    }
+});
+
+app.post("/Sign-Up-Student", async(req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+        const newUser = new User({
+            FirstName: req.body.FirstName,
+            LastName: req.body.LastName,
+            id: req.body.id,
+            password: hashedPassword,
+            email: req.body.email,
+            Gender: req.body.Gender,
+            Age: req.body.Age,
+            Phone: req.body.Phone,
+            Roll: req.body.Roll,
+            Birthdate: req.body.Birthdate
+        });
+
+        const existingUser = await User.findOne({ FirstName: req.body.FirstName });
+
+        if (existingUser) {
+            console.log("User already exists!");
+            return res.redirect("/Sign-Up-Student");
+        }
+
+        if (!passwordSchema.validate(req.body.password)) {
+            console.log("Password validation failed");
+            return res.redirect("/Sign-Up-Student");
+        }
+
+        await newUser.save();
+        console.log("Sign up successful");
+        return res.redirect('/Sign-Up-Student');
+
+    } catch (err) {
+        console.error('Error signing up:', err);
+        return res.redirect('/Sign-Up-Student');
+    }
+});
+
+app.post('/Sign-In', async(req, res) => {
+    try {
+        const { password, FirstName } = req.body;
+
+        const user = await User.findOne({ FirstName }).exec();
+
+        if (user && await bcrypt.compare(password, user.password)) {
+            if (user.Roll === "Student") {
+                res.cookie('user', JSON.stringify(user), { httpOnly: true });
+                console.log("Sign in successful");
+                return res.redirect('/Student-Profile');
+            }
+            if (user.Roll === "Freelancer") {
+                res.cookie('user', JSON.stringify(user), { httpOnly: true });
+                console.log("Sign in successful");
+                return res.redirect('/Freelancer-Profile');
+            }
+            if (user.Roll === "Admin") {
+                res.cookie('user', JSON.stringify(user), { httpOnly: true });
+                console.log("Sign in successful");
+                return res.redirect('/Admin-Profile');
+            }
+        } else {
+            console.log("Invalid credentials");
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).redirect('/Sign-In');
+    }
+});
+
+
 
 
 module.exports = app;
